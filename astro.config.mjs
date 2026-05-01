17992a065148b0d4ed8e3d3f5190fbcac2280003
TheLittleQuteGDH <thelittlequtegdh@foxmail.com>
2026-05-01 12:09:09 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog.thelittlequtegdh.dpdns.org/",
+	site: "https://blog.gdh.us.kg",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
